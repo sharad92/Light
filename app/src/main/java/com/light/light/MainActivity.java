e4b8344133d225da24eddfd711f8834552df7763
Sharad Srivastava <sharadsrivastava92@gmail.com>
2016-05-16 20:03:16 +0530

    secondary commit

--- app/src/main/java/com/light/light/MainActivity.java
+++ app/src/main/java/com/light/light/MainActivity.java
@@ -6,13 +6,14 @@ import android.hardware.Camera;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     ToggleButton tButton;
-    Camera camera;
+    static Camera camera = null;
     Camera.Parameters para;
     boolean isFlash = false;
     boolean isOn = false;
@@ -23,53 +24,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tButton = (ToggleButton) findViewById(R.id.btnSwitch);
 
-        //To check camera availability
-        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
-            camera=Camera.open();
-            para=camera.getParameters();
-            isFlash = true;
-        }
-        tButton.setOnClickListener(new View.OnClickListener() {
+        // int permissionCheck = ContextCompat.checkSelfPermission(android.permission.CAMERA);
 
-            public void onClick(View v) {
-                if (isFlash) {
-                    //Turn flash on/off
-                    if (!isOn) {
-                        para.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                        camera.setParameters(para);
-                        camera.startPreview();
-                        isOn = true;
-                    } else {
-                        para.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                        camera.setParameters(para);
-                        camera.stopPreview();
-                        isOn = false;
-                    }
-                } else {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Error");
-                    builder.setMessage("Flash not available");
-                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                            finish();
-                        }
-                    });
-                    AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-                }
+        try {
+            //To check camera availability
+            if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+                camera = Camera.open();
+                para = camera.getParameters();
+                isFlash = true;
             }
-           });
+            tButton.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+                    if (isFlash) {
+                        //Turn flash on/off
+                        if (!isOn) {
+                            para.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                            camera.setParameters(para);
+                            camera.startPreview();
+                            isOn = true;
+                        } else {
+                            para.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                            camera.setParameters(para);
+                            camera.stopPreview();
+                            isOn = false;
+                        }
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("Error");
+                        builder.setMessage("Flash not available");
+                        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                finish();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            Log.e("Error", "" + e);
+        }
     }
 
-        protected void onStop(){
+    protected void onStop() {
         super.onStop();
-        if(camera!=null){
+        if (camera != null) {
             camera.release();
             camera = null;
         }
+        super.onPause();
     }
+
 
 }
 
